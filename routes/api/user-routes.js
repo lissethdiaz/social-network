ae9962a5d6090a1cd94e3fde4b0c8063c29c1fd5
Liss Diaz <lissdiaz15@gmail.com>
2022-03-22 19:13:47 -0600

    user routes complete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,29 @@
 const router = require('express').Router();
 
 const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/user-controller');
+
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:id/friends/friendsId')
+  .post(addFriend)
+  .delete(removeFriend)
 
 module.exports = router;
